670072a4de13a8ba24ed73b9d7a45edeaed0582a
Harsh Patil <harshpatilmac@Harshs-MacBook-Air.local>
2025-01-05 18:45:56 +0530

    fixes

--- loggedin.js
+++ loggedin.js
@@ -2,14 +2,15 @@ let users = JSON.parse(localStorage.getItem("users"))
 console.log(users);
 
 let currentuser = ""
+let previoususer = ""
 
 users.forEach(user => {
     if (user.loggedIn === true) {
         if (currentuser === ""){
-            currentuser = user.name
+            currentuser = user
         }
         else{
-            currentuser.loggedIn = "false"
+            currentuser.loggedIn = false
             currentuser = user.name
         }
         console.log(user)
